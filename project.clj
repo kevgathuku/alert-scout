84ed2276228acad47d07f7a53461a852a08a4431
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-12-31 22:33:57 +0100

    add rebel readline repl

--- project.clj
+++ project.clj
@@ -4,11 +4,13 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.2"]
+                 [com.bhauman/rebel-readline "0.1.5"]
                  [com.rometools/rome "2.1.0"]
                  [clj-http "3.13.1"]
                  [metosin/malli "0.16.4"]]
   :main ^:skip-aot my-stuff.core
   :target-path "target/%s"
+  :aliases {"rebl" ["trampoline" "run" "-m" "rebel-readline.main"]}
   :plugins [[cider/cider-nrepl "0.58.0"] [lein-cljfmt "0.9.2"]]
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
