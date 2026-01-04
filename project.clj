3e335a41c3ee3ec7f0a0d51269c15c4a2d19c182
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2026-01-04 21:58:26 +0100

    Finalize changes
    - remove leftover tests
    - add project name and description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject my-stuff "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject alert-scout "0.1.0-SNAPSHOT"
+  :description "Monitoring alerting and reporting system for RSS/Atom feeds"
   :url "https://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
